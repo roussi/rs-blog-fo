3b25f74df70deb6149376e7684834b88f270a530
A. Roussi <roussi.abdelghani@threedsecure.com>
2019-04-13 21:55:52 +0200

    add twitter data and style

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent implements OnInit {
+export class ContactComponent implements OnDestroy {
+  private twitter: any;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private _router: Router) {
+    this.initTwitterWidget();
   }
 
+  initTwitterWidget() {
+    this.twitter = this._router.events.subscribe(val => {
+      if (val instanceof NavigationEnd) {
+        (<any>window).twttr = (function (d, s, id) {
+          let js: any, fjs = d.getElementsByTagName(s)[0],
+              t = (<any>window).twttr || {};
+          if (d.getElementById(id)) return t;
+          js = d.createElement(s);
+          js.id = id;
+          js.src = "https://platform.twitter.com/widgets.js";
+          fjs.parentNode.insertBefore(js, fjs);
+
+          t._e = [];
+          t.ready = function (f: any) {
+              t._e.push(f);
+          };
+
+          return t;
+        }(document, "script", "twitter-wjs"));
+        if ((<any>window).twttr.ready())
+          (<any>window).twttr.widgets.load();
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.twitter.unsubscribe();
+  }
 }
--- src/app/shared/side-bar/side-bar.component.ts
+++ src/app/shared/side-bar/side-bar.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
   styleUrls: ['./side-bar.component.scss']
 })
-export class SideBarComponent implements OnInit {
+export class SideBarComponent implements OnDestroy {
+  private twitter: any;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private _router: Router) {
+    this.initTwitterWidget();
   }
 
+  initTwitterWidget() {
+    this.twitter = this._router.events.subscribe(val => {
+      if (val instanceof NavigationEnd) {
+        (<any>window).twttr = (function (d, s, id) {
+          let js: any, fjs = d.getElementsByTagName(s)[0],
+              t = (<any>window).twttr || {};
+          if (d.getElementById(id)) return t;
+          js = d.createElement(s);
+          js.id = id;
+          js.src = "https://platform.twitter.com/widgets.js";
+          fjs.parentNode.insertBefore(js, fjs);
+
+          t._e = [];
+          t.ready = function (f: any) {
+              t._e.push(f);
+          };
+
+          return t;
+        }(document, "script", "twitter-wjs"));
+        if ((<any>window).twttr.ready())
+          (<any>window).twttr.widgets.load();
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.twitter.unsubscribe();
+  }
 }
